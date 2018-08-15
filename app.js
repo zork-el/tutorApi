3e46c1173a172ab3a3c3d3dc9751539482cabd3b
Prithul Singh <ccarmichael.zork.el@gmail.com>
2018-08-15 23:31:15 +0530

    Added session instead of cookie!

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var fileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/userRouter');
@@ -32,12 +34,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new fileStore()
+}))
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
 
     if (!authHeader) {
@@ -47,13 +57,13 @@ function auth(req, res, next) {
       return next(err);
     }
 
-    var authUserPass = authHeader.toString().split(' ')[1]  //gets string containing username&password.
-    var auth = Buffer.from(authUserPass, 'base64').toString().split(':');    //splits authUserPass into array containing username&password.
+    var authUserPass = authHeader.toString().split(' ')[1]  //gets string containing username:password.
+    var auth = Buffer.from(authUserPass, 'base64').toString().split(':');    //splits authUserPass into array containing username:password.
     var username = auth[0];
     var password = auth[1];
 
     if (username === 'admin' && password === 'admin') {
-      res.cookie('user', 'admin', { signed: true });
+      req.session.user = 'admin';
       next();
     } else {
       var err = new Error("You are Not Authenticated!");
@@ -63,7 +73,7 @@ function auth(req, res, next) {
     }
   }
   else {
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       next();
     } else {
       var err = new Error("You are Not Authenticated!");
